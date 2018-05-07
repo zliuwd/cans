114b8882a71472b49928cf7bd5f8452584c67a0c
jon walker <jonathan.b.walker0@gmail.com>
2018-05-07 16:25:39 -0700

    CANS-155 global header (#4)
    * add reactstrap
    
    * update title
    
    * * add bootstrap
    * add minimal layout from reactstrap
    
    * * add react-wood-duck 0.1.57 (includes dist/fonts fix)
    * add GlobalHeader and PageHeader

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import { GlobalHeader, PageHeader } from 'react-wood-duck'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Container>
-        <Row>
-          <Col xs="3"></Col>
-          <Col xs="auto"></Col>
-          <Col xs="3"></Col>
-        </Row>
-      </Container>
+      <Fragment>
+        <GlobalHeader profileName="Username" profileId="profile.id"/>
+        <PageHeader pageTitle={'CANS Assessment Application'} button={null}/>
+        <Container>
+          <Row>
+            <Col xs="3"></Col>
+            <Col xs="auto"></Col>
+            <Col xs="3"></Col>
+          </Row>
+        </Container>
+      </Fragment>
     );
   }
 }
