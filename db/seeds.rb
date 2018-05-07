6fb49717dbc8d663769d62dbdd218852b271b93b
Ed Gibbs <edward_gibbs@yahoo.com>
2018-05-07 06:49:14 -0700

    Creates seed data for default template

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+construct = Construct.create(name: 'Default')
+Domain.create(title: 'CHILD BEHAVIORAL/EMOTIONAL NEEDS', construct_id: construct.id)
+Domain.create(title: 'LIFE DOMAIN FUNCTIONING', construct_id: construct.id)
+Domain.create(title: 'RISK BEHAVIORS', construct_id: construct.id)
+Domain.create(title: 'CULTURAL FACTORS', construct_id: construct.id)
+Domain.create(title: 'STRENGTHS DOMAIN', construct_id: construct.id)
+Domain.create(title: 'CAREGIVER RESOURCES & NEEDS', construct_id: construct.id)
+Domain.create(title: 'TRAUMA', construct_id: construct.id)
