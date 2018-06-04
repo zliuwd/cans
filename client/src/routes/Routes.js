1c14b79c82cb248f06f764a564206b2662990b21
jon walker <jonathan.b.walker0@gmail.com>
2018-06-04 12:58:29 -0700

    remove duplicate router from app (#45)

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Assessment, ClientsContainer, Client } from '../components';
+import { Route, Switch } from 'react-router-dom';
+import { Assessment, Client, ClientsContainer } from '../components';
 import { ChildForm } from '../components/ChildForm';
 
 class Routes extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/assessments" component={Assessment} />
-          <Route exact path="/clients/new" component={ChildForm} />
-          <Route path="/clients/:id" component={Client} />
-          <Route exact path="/" component={ClientsContainer}/>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/assessments" component={Assessment} />
+        <Route exact path="/clients/new" component={ChildForm} />
+        <Route path="/clients/:id" component={Client} />
+        <Route exact path="/" component={ClientsContainer} />
+      </Switch>
     );
   }
 }
