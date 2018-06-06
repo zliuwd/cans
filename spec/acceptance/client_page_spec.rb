916e4d86e4b6867ec9a90dc36073c31da7c397c5
Ed Gibbs <edward_gibbs@yahoo.com>
2018-06-06 15:16:17 -0700

    Revert "Update acceptance test" (#48)
    This reverts commit 6dc6579309af579a6aabf35da3eebaad4a96398a.

--- spec/acceptance/client_page_spec.rb
+++ spec/acceptance/client_page_spec.rb
@@ -8,8 +8,7 @@ require 'active_support/time'
 feature 'Client Pages' do
   given(:first_name) { Faker::Name.first_name }
   given(:last_name) { Faker::Name.last_name }
-  # given(:date_of_birth) { Faker::Date.between(20.years.ago, 10.years.ago) }
-  given(:date_of_birth) { '01/01/1970' }
+  given(:date_of_birth) { Faker::Date.between(20.years.ago, 10.years.ago) }
   given(:case_number) { Faker::Number.number(10) }
 
   scenario 'can add a new client' do
@@ -17,14 +16,15 @@ feature 'Client Pages' do
     visit '/clients/new'
     expect(page).to have_content 'Add Child/Youth'
 
+    fill_in('First Name', with: '')
+    click_button('Save')
+    expect(page).to have_content 'Add Child/Youth'
+
     fill_in('First Name', with: first_name)
     fill_in('Last Name', with: last_name)
     page.find('#dob').set(date_of_birth)
-    page.find('#dob').native.send_keys(:up)
-    page.find('#dob').native.send_keys(:down)
     fill_in('Case Number', with: case_number)
     find('div[aria-haspopup=true][role=button]').click
-    expect(find_button('Save', disabled: true).disabled?).to be true
     find('li', text: 'Fresno').send_keys(:enter)
     click_button 'Save'
     expect(page).to have_content 'Child/Youth Profile'
