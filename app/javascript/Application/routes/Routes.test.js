0ee2379d64e0f37ab3fd0cb3fc6c43e99e6c39d0
jon walker <jonathan.b.walker0@gmail.com>
2018-07-23 13:56:30 -0700

    Cans 187 jest config (#85)
    * prettier file
    
    * restrict coverage to specific js files
    
    * set test coverage threshold to 70 percent
    
    * set test coverage threshold to 90 percent for Assessment dir
    
    * additional coverage for Router.js
    
    * add statements to global coverage threshold

--- app/javascript/Application/routes/Routes.test.js
+++ app/javascript/Application/routes/Routes.test.js
@@ -1,28 +1,55 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import { executeTest } from './Routes.test.util';
 import { AssessmentContainer } from '../components/Assessment';
 import { ClientsContainer } from '../components/Client';
 import { ChildForm } from '../components/ChildForm';
+import { Routes } from './';
+import { Route, Switch } from 'react-router-dom';
 
-/**
- * To test that a configured route is working, create a new
- * it block below and pass executeTest() the component, the url and some html
- * that will be rendered from the component.
- *
- * example: the following route is defined in Routes.js:
- *   <Route path="/home" component={Home} />
- *
- * so you will pass the following:
- *   executeTest(Home, /home', 'Welcome!')
- */
+describe('<Router />', () => {
+  describe('#render', () => {
+    const getWrapper = () => shallow(<Routes />);
+    const getLength = component => getWrapper().find(component).length;
 
-it('navigates to assessments', () => {
-  executeTest(AssessmentContainer, '/assessments', 'Communimetric');
-});
+    it('renders with 1 <Switch /> component', () => {
+      console.log(getWrapper().debug());
+      expect(getLength(Switch)).toBe(1);
+    });
 
-it('navigates to clients list page', () => {
-  executeTest(ClientsContainer, '/', '');
-});
+    it('renders with 4 <Route /> component', () => {
+      expect(getLength(Route)).toBe(4);
+    });
+  });
 
-it('navigates to clients form page', () => {
-  executeTest(ChildForm, '/clients/new', 'Child/Youth');
+  /**
+   * To test that a configured route is working, create a new
+   * test block below and pass executeTest() the component, the url and some html
+   * that will be rendered from the component.
+   *
+   * example: the following route is defined in Routes.js:
+   *   <Route path="/home" component={Home} />
+   *
+   * so you will pass the following:
+   *   executeTest(Home, /home', 'Welcome!')
+   */
+  describe('route config', () => {
+    describe('/', () => {
+      it('navigates to clients list page', () => {
+        executeTest(ClientsContainer, '/', '');
+      });
+    });
+
+    describe('/assessments', () => {
+      it('navigates to assessments', () => {
+        executeTest(AssessmentContainer, '/assessments', 'Communimetric');
+      });
+    });
+
+    describe('/clients/new', () => {
+      it('navigates to clients form page', () => {
+        executeTest(ChildForm, '/clients/new', 'Child/Youth');
+      });
+    });
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,27 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
   verbose: true,
-  rootDir: "./app/javascript",
-  setupTestFrameworkScriptFile: "<rootDir>/setupTests.js",
-  coverageDirectory: path.resolve(__dirname, "coverage", "javascript"),
+  rootDir: './app/javascript',
+  setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
+  coverageDirectory: path.resolve(__dirname, 'coverage', 'javascript'),
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.(css|scss|sass)$": "identity-obj-proxy"
-  }
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|scss|sass)$': 'identity-obj-proxy',
+  },
+  collectCoverageFrom: ['**/*.js', '!**/index.js', '!**/packs/**'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+    './app/javascript/Application/components/Assessment': {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
