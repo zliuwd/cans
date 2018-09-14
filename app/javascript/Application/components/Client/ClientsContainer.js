aace7fbc70116488c7d8436552832a19d17ec250
Denys Davydov <denys.davydov@osi.ca.gov>
2018-09-14 10:02:38 -0700

    CANS-309. Fix accessibility issues. Fix a11y tests

--- app/javascript/Application/components/Client/ClientsContainer.js
+++ app/javascript/Application/components/Client/ClientsContainer.js
@@ -1,41 +1,94 @@
 import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import ClientService from './Client.service';
-import { groupClientsByLastName, formatClientName } from './Client.helper';
+import { formatClientName } from './Client.helper';
 import Button from '@material-ui/core/Button/Button';
 import Card from '@material-ui/core/Card/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
+import DataGrid from '@cwds/components/lib/DataGrid';
 
 import './style.sass';
 
-/* eslint-disable camelcase */
+const calculatePages = (recordsCount, pageSize) => {
+  let pages = Math.floor(recordsCount / pageSize);
+  pages += recordsCount % pageSize > 0 ? 1 : 0;
+  return pages;
+};
+
 class ClientsContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clients: [],
-      clients_status: 'idle',
+      pagination: {
+        page: 0,
+        pages: 0,
+        pageSize: 10,
+        records: [],
+      },
+      clientsAll: [],
+      clientsStatus: 'ready',
     };
   }
 
-  componentDidMount() {
-    this.fetchAllClients();
+  async componentDidMount() {
+    await this.fetchAllClients();
   }
 
   fetchAllClients = () => {
-    this.setState({ clients_status: 'waiting' });
+    this.setState({ clientsStatus: 'waiting' });
     return ClientService.fetchAllClients()
       .then(this.onFetchAllClientsSuccess)
-      .catch(() => this.setState({ clients_status: 'error' }));
+      .catch(() => this.setState({ clientsStatus: 'error' }));
   };
 
-  onFetchAllClientsSuccess = clients => {
-    const groupedClients = groupClientsByLastName(clients);
+  onFetchAllClientsSuccess = clientsAll => {
+    const pagination = this.state.pagination;
+    const pages = calculatePages(clientsAll.length, pagination.pageSize);
     this.setState({
-      clients: groupedClients,
-      clients_status: 'ready',
+      pagination: {
+        ...pagination,
+        pages,
+      },
+      clientsAll: clientsAll,
+      clientsStatus: 'ready',
+    });
+    this.simulateFetchData(pagination);
+  };
+
+  handleOnPageChange = page => {
+    const { pagination } = this.state;
+    this.simulateFetchData({
+      ...pagination,
+      page,
+    });
+  };
+
+  handleOnPageSizeChange = pageSize => {
+    const { pagination, clientsAll } = this.state;
+    const pages = calculatePages(clientsAll.length, pageSize);
+    this.simulateFetchData({
+      ...pagination,
+      page: 0,
+      pages,
+      pageSize,
+    });
+  };
+
+  simulateFetchData = pagination => {
+    const { clientsAll } = this.state;
+    const { page, pageSize } = pagination;
+    this.setState({ clientsStatus: 'waiting' });
+    const records = clientsAll.slice(page * pageSize, (page + 1) * pageSize);
+    const newPagination = {
+      ...pagination,
+      records,
+    };
+    console.log(JSON.stringify(newPagination));
+    this.setState({
+      pagination: newPagination,
+      clientsStatus: 'ready',
     });
   };
 
@@ -49,17 +102,6 @@ class ClientsContainer extends Component {
     );
   }
 
-  renderClientsRecord = record => {
-    return (
-      <div className="client-name-container" key={record.letter}>
-        <div className="header">{record.letter}</div>
-        {record.clients.map(client => {
-          return this.renderClientName(client);
-        })}
-      </div>
-    );
-  };
-
   renderClientName = client => {
     return (
       <div className="client-name" key={client.id}>
@@ -68,16 +110,21 @@ class ClientsContainer extends Component {
     );
   };
 
-  renderNoData = () => {
-    if (this.state.clients_status === 'ready' && (this.state.clients || []).length === 0) {
-      return <div id="no-data">No clients found</div>;
-    } else {
-      return null;
-    }
-  };
-
   render = () => {
-    const clients = this.state.clients || [];
+    const { pagination, clientsStatus } = this.state;
+    const { records, page, pages, pageSize } = pagination;
+    const columns = [
+      {
+        id: 'fullName',
+        Header: 'Full Name',
+        accessor: client => this.renderClientName(client),
+      },
+      {
+        Header: 'Date of Birth',
+        accessor: 'dob',
+      },
+    ];
+    const loading = clientsStatus === 'waiting';
     return (
       <Fragment>
         <Grid item className={'client-grid'} xs={12}>
@@ -87,20 +134,30 @@ class ClientsContainer extends Component {
               title="County Client List"
               action={this.renderAddChildButton()}
             />
-            <CardContent>
-              <div className="clients-container">
-                {clients.map(record => {
-                  return this.renderClientsRecord(record);
-                })}
-                {this.renderNoData()}
-              </div>
-            </CardContent>
+            <div className={'content'}>
+              <CardContent>
+                <DataGrid
+                  manual
+                  data={records}
+                  page={page}
+                  pages={pages || 4}
+                  pageSize={pageSize}
+                  columns={columns}
+                  minRows={0}
+                  style={{ paddingBottom: '15rem' }}
+                  loading={loading}
+                  noDataText={'No clients found'}
+                  // onFetchData={this.fetchData}
+                  onPageChange={this.handleOnPageChange}
+                  onPageSizeChange={this.handleOnPageSizeChange}
+                />
+              </CardContent>
+            </div>
           </Card>
         </Grid>
       </Fragment>
     );
   };
 }
-/* eslint-enable camelcase */
 
 export default ClientsContainer;
