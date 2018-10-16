bbc581d68800b1dc3bbf42f227fcc92daf4f775e
amanbop <aman_boparai@yahoo.com>
2018-10-16 14:37:21 -0700

    Fixed the No Cache-control and Pragma HTTP Header Set issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_no_cache
   def fallback_index_html
     @data = { foo: 'bar' }
+  end
+
+  def set_no_cache
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
   end
 end
