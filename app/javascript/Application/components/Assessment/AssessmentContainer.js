0c3abc0878b2cce79acb4c4e9f569cea96f5c1fc
Shravan Parande <sparande@cambriasolutions.com>
2018-10-16 05:51:38 -0700

    Cans-375 fix validating leap year dates

--- app/javascript/Application/components/Assessment/AssessmentContainer.js
+++ app/javascript/Application/components/Assessment/AssessmentContainer.js
@@ -240,7 +240,7 @@ class AssessmentContainer extends Component {
   );
 
   validateDate(date) {
-    const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+    const dateRegex = /^(?:(?:0?[13578]|1[02])|(?:0?[469]|11)(?!\/31)|(?:0?2)(?:(?!\/3[01]|\/29\/(?:(?:0[^48]|[13579][^26]|[2468][^048])00|(?:\d{2}(?:0[^48]|[13579][^26]|[2468][^048]))))))\/(?:0?[1-9]|[12][0-9]|3[01])\/\d{4}$/g;
     return dateRegex.test(date);
   }
   handleKeyUp = date => {
