3beb68377f2f7c586b7b01e68add71e394b34567
money <manmohit.singh@gmail.com>
2018-12-04 15:24:38 -0800

    Remove: Cans logic

--- config/initializers/cwds_authentication.rb
+++ config/initializers/cwds_authentication.rb
@@ -1,5 +1,4 @@
 Cwds::Authentication.configure do |config|
   config.authentication_enabled   = ENV.fetch('AUTHENTICATION_ENABLED', true)
   config.perry_base_url           = Rails.configuration.micro_services['perry_base_url']
-  config.cans_base_path           = Rails.configuration.micro_services['cans_api_base_url']
 end
