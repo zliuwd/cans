ab6d5d26af05a1b934e087ecf2f96cfaefd4dd2d
Nasar-cambria <nkprodware@gmail.com>
2018-12-12 14:56:52 -0800

    clean up

--- app/javascript/Application/components/Print/PrintAssessment.test.js
+++ app/javascript/Application/components/Print/PrintAssessment.test.js
@@ -10,15 +10,7 @@ import {
 
 describe('<PrintAssessment />', () => {
   it('should match fixture as plain html', () => {
-    const printAssessment = shallow(
-      <PrintAssessment
-        assessment={assessmentPrint}
-        i18n={i18nPrint}
-        isUnderSix={() => {
-          return { key: 'value' }
-        }}
-      />
-    )
+    const printAssessment = shallow(<PrintAssessment assessment={assessmentPrint} i18n={i18nPrint} />)
     const actualHtmlRaw = printAssessment.html()
     const actualHtml = actualHtmlRaw.replace(
       /<span style="text-align:right;font-style:italic">[^>]+<\/span>/g,
@@ -28,15 +20,7 @@ describe('<PrintAssessment />', () => {
   })
 
   it('should render confidential item and redact comment and rating when confidential by default and confidential is true', () => {
-    const printConfidential = shallow(
-      <PrintAssessment
-        assessment={assessmentWithConfidentialItem}
-        i18n={i18nPrint}
-        isUnderSix={() => {
-          return { key: 'value' }
-        }}
-      />
-    )
+    const printConfidential = shallow(<PrintAssessment assessment={assessmentWithConfidentialItem} i18n={i18nPrint} />)
     const printConfidentialHtml = printConfidential.html()
     expect(printConfidentialHtml).toContain('Confidential')
     expect(printConfidentialHtml).toContain('<div style="width:7.6rem"></div>') // Redacted rating
@@ -44,15 +28,7 @@ describe('<PrintAssessment />', () => {
   })
 
   it('should redact domain comment when it contains at least one item with confidential by default and confidential is true', () => {
-    const printConfidential = shallow(
-      <PrintAssessment
-        assessment={assessmentWithConfidentialItem}
-        i18n={i18nPrint}
-        isUnderSix={() => {
-          return { key: 'value' }
-        }}
-      />
-    )
+    const printConfidential = shallow(<PrintAssessment assessment={assessmentWithConfidentialItem} i18n={i18nPrint} />)
     const printConfidentialHtml = printConfidential.html()
     expect(printConfidentialHtml).not.toContain('This domain comment is not supposed to be printed')
   })
@@ -60,15 +36,7 @@ describe('<PrintAssessment />', () => {
   it('should not crash if the assessment has no county', () => {
     const assessment = { ...assessmentPrint, county: undefined }
     expect(() => {
-      shallow(
-        <PrintAssessment
-          assessment={assessment}
-          i18n={i18nPrint}
-          isUnderSix={() => {
-            return { key: 'value' }
-          }}
-        />
-      )
+      shallow(<PrintAssessment assessment={assessment} i18n={i18nPrint} />)
     }).not.toThrow()
   })
 })
