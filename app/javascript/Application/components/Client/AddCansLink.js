39e97f5fad1683848fbc5cc5176458533e63aaa6
Gustavo Gomez <ggomez@cambriasolutions.com>
2018-12-12 10:33:10 -0800

    lint fix

--- app/javascript/Application/components/Client/AddCansLink.js
+++ app/javascript/Application/components/Client/AddCansLink.js
@@ -21,10 +21,7 @@ class AddCansLink extends Component {
     return this.isDisabled() ? (
       this.renderLinkText()
     ) : (
-      <Link
-        id={'add-cans-link'}
-        to={`/clients/${clientIdentifier}/assessments`}
-      >
+      <Link id={'add-cans-link'} to={`/clients/${clientIdentifier}/assessments`}>
         {this.renderLinkText()}
       </Link>
     )
