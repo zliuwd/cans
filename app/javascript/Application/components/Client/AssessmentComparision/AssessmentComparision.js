6519d23d6f8e0e2641e1ea1b2916ddf477f38aff
JasonLiu <json.liu@osi.ca.gov>
2019-02-10 04:58:46 -0800

    change card title

--- app/javascript/Application/components/Client/AssessmentComparision/AssessmentComparision.js
+++ app/javascript/Application/components/Client/AssessmentComparision/AssessmentComparision.js
@@ -12,37 +12,12 @@ class AssessmentComparision extends Component {
   render() {
     const data = this.state.data
     const counter = new Array(this.state.data.date_info.length)
-
-    // outer table, data from domians
-
-    // inner table without header, data from items
-    // const itemNameCol = {
-    //   id: 'itemName',
-    //   accessor: item => {
-    //     return item.code
-    //   },
-    // }
-    //
-    // const itemRatingColsGenerator = () => {
-    //   counter.map((el, index) => {
-    //     return {
-    //       id: `${el.item_id}-${index}`,
-    //       accessor: item => {
-    //         return item.item_ratings[index].value
-    //       },
-    //       Cell: comparisionItemRatingsTemp,
-    //     }
-    //   })
-    // }
-    //
-    // const itemCmparisionTableTemplate = [itemNameCol, ...itemRatingColsGenerator()]
-
     return (
       <Fragment>
         <Grid item xs={12}>
           <Card className="card-cans-client-list">
             <CardHeader>
-              <CardTitle className={'card-title-fix'}>Assessment Comparison Table</CardTitle>
+              <CardTitle className={'card-title-fix'}>Assessment Comparision Table</CardTitle>
             </CardHeader>
             <CardBody>
               <ComparisionOuterTable data={data} counter={counter} />
