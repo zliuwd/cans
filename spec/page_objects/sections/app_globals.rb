4251915a284a16adedbe893426479810f3818ed9
DmitriyRudenko <Dmitry.Rudenko@osi.ca.gov>
2019-02-13 17:03:33 -0800

    unsaved assessment validation on print

--- spec/page_objects/sections/app_globals.rb
+++ spec/page_objects/sections/app_globals.rb
@@ -8,4 +8,7 @@ class AppGlobals < SitePrism::Section
   elements:reason_select_options, '.list__option'
   element :cancel_button_of_warning, 'button.warning-modal-logout'
   element :agree_button_of_warning, 'button.warning-modal-stay-logged-in'
+  element :return_to_the_assessment_button, 'button', text: 'Return to the assessment'
+  element :save_and_continue_button, 'button', text: 'SAVE CHANGES AND CONTINUE'
+  element :discard_and_continue_button, 'button', text: 'Discard changes and continue'
 end
