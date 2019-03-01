c8b673c25d201d7c9e3882a08656c416d8b1aea2
Will Mitchell <will.mitchell@osi.ca.gov>
2019-03-01 13:46:50 -0800

    Track Assessment changes in AssessmentDraftManager

--- app/javascript/Application/components/Assessment/AssessmentDraftManager.js
+++ app/javascript/Application/components/Assessment/AssessmentDraftManager.js
@@ -1,30 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { LoadingState } from '../../util/loadingHelper'
 
-class AssessmentDraftManager extends React.Component {
+class AssessmentDraftManager extends React.PureComponent {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      assessment: props.assessment,
+    }
   }
 
-  static getDerivedStateFromProps(props, state) {
-    if (props.assessment !== state.assessment) {
-      return { assessment: props.assessment }
-    }
-    return null
+  handleResetAssessment = () => {
+    this.setState({ assessment: this.props.assessment })
+  }
+
+  handleSaveAssessment = () => {
+    this.props.onSave(this.state.assessment)
+  }
+
+  handleSetAssessment = assessment => {
+    this.setState({ assessment })
   }
 
   render() {
-    return React.cloneElement(this.props.children, { assessment: this.state.assessment })
+    return React.cloneElement(this.props.children, {
+      assessment: this.state.assessment,
+      loadingState: this.props.loadingState,
+      onResetAssessment: this.handleResetAssessment,
+      onSaveAssessment: this.handleSaveAssessment,
+      onSetAssessment: this.handleSetAssessment,
+    })
   }
 }
 
 AssessmentDraftManager.propTypes = {
   assessment: PropTypes.object,
   children: PropTypes.element.isRequired,
+  loadingState: PropTypes.oneOf(Object.values(LoadingState)),
+  onSave: PropTypes.func.isRequired,
 }
 
 AssessmentDraftManager.defaultProps = {
   assessment: null,
+  loadingState: LoadingState.waiting,
 }
 export default AssessmentDraftManager
--- app/javascript/Application/components/Assessment/AssessmentDraftManager.test.js
+++ app/javascript/Application/components/Assessment/AssessmentDraftManager.test.js
@@ -2,24 +2,98 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import AssessmentDraftManager from './AssessmentDraftManager'
 import * as AssessmentMocks from './assessment.mocks.test'
+import { LoadingState } from '../../util/loadingHelper'
 
 const MyComponent = () => 'Hello'
 
 describe('AssessmentDraftManager', () => {
-  const render = () =>
+  const initialAssessment = AssessmentMocks.assessment
+
+  const render = (onSave = () => {}) =>
     shallow(
-      <AssessmentDraftManager assessment={AssessmentMocks.assessment}>
+      <AssessmentDraftManager assessment={initialAssessment} loadingState={LoadingState.ready} onSave={onSave}>
         <MyComponent />
       </AssessmentDraftManager>
     )
 
+  const findChild = wrapper => wrapper.find(MyComponent)
+
   it('saves the assessment into state to track local changes', () => {
     const wrapper = render()
-    expect(wrapper.state().assessment).toEqual(AssessmentMocks.assessment)
+    expect(wrapper.state().assessment).toEqual(initialAssessment)
   })
 
-  it('passes the draft assessment down to children', () => {
+  it('passes the assessment down to the child', () => {
     const wrapper = render()
-    expect(wrapper.find(MyComponent).props().assessment).toEqual(AssessmentMocks.assessment)
+    expect(findChild(wrapper).props().assessment).toEqual(initialAssessment)
+  })
+
+  it('passes the loading state down to the child', () => {
+    const wrapper = render()
+    expect(findChild(wrapper).props().loadingState).toBe(LoadingState.ready)
+  })
+
+  it('passes the draft assessment when changes have occurred', () => {
+    const wrapper = render()
+
+    const updatedAssessment = {
+      ...initialAssessment,
+      can_release_confidential_info: !initialAssessment.can_release_confidential_info,
+    }
+    wrapper.setState({ assessment: updatedAssessment })
+    wrapper.update()
+
+    expect(findChild(wrapper).props().assessment).toEqual(updatedAssessment)
+    expect(findChild(wrapper).props().assessment).not.toEqual(initialAssessment)
+  })
+
+  it('provides a callback to modify the draft assessment', () => {
+    const wrapper = render()
+
+    const updatedAssessment = {
+      ...initialAssessment,
+      can_release_confidential_info: !initialAssessment.can_release_confidential_info,
+    }
+
+    findChild(wrapper)
+      .props()
+      .onSetAssessment(updatedAssessment)
+
+    expect(wrapper.state().assessment).toEqual(updatedAssessment)
+  })
+
+  it('provides a callback to reset the draft state to what is persisted', () => {
+    const wrapper = render()
+
+    const updatedAssessment = {
+      ...initialAssessment,
+      can_release_confidential_info: !initialAssessment.can_release_confidential_info,
+    }
+    wrapper.setState({ assessment: updatedAssessment })
+    wrapper.update()
+
+    findChild(wrapper)
+      .props()
+      .onResetAssessment(initialAssessment)
+
+    expect(wrapper.state().assessment).toEqual(initialAssessment)
+  })
+
+  it('provides a callback to persist the draft state', () => {
+    const handleSave = jest.fn()
+    const wrapper = render(handleSave)
+
+    const updatedAssessment = {
+      ...initialAssessment,
+      can_release_confidential_info: !initialAssessment.can_release_confidential_info,
+    }
+    wrapper.setState({ assessment: updatedAssessment })
+    wrapper.update()
+
+    findChild(wrapper)
+      .props()
+      .onSaveAssessment()
+
+    expect(handleSave).toHaveBeenCalledWith(updatedAssessment)
   })
 })
