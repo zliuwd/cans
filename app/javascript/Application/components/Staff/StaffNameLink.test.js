2ea8407dfe5164e44b5394f49a1d53785eeb9e9f
Gurpartap S Gill <gurpartap.gill@osi.ca.gov>
2019-03-11 15:26:30 -0700

    CANS-1023-DisbleStaffNameIfZeroClients (#567)
    * disabling link off staff name with zero clients on supervisor dash
    
    * Removing unnecessary span

--- app/javascript/Application/components/Staff/StaffNameLink.test.js
+++ app/javascript/Application/components/Staff/StaffNameLink.test.js
@@ -7,16 +7,27 @@ describe('<StaffNameLink />', () => {
   const render = (original, row) => shallow(<StaffNameLink row={row} original={original} />)
 
   it('renders a link to the case load dashboard of the person', () => {
-    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory' })
+    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory', clients_count: 1 })
     const link = wrapper.find(Link)
     expect(link.exists()).toBe(true)
     expect(link.props().to).toBe('/staff/ABC')
   })
 
   it('renders the accessed name as the text of the link', () => {
-    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory' })
+    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory', clients_count: 1 })
     const link = wrapper.find(Link)
     expect(link.exists()).toBe(true)
     expect(link.props().children).toBe('Cassel, Jory')
+  })
+
+  it('does not render a link to case load if staff has 0 clients', () => {
+    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory', clients_count: 0 })
+    const link = wrapper.find(Link)
+    expect(link.exists()).toBe(false)
+  })
+
+  it('renders the staff name as simple text if staff has 0 clients', () => {
+    const wrapper = render({ staff_person: { identifier: 'ABC' } }, { staffName: 'Cassel, Jory', clients_count: 0 })
+    expect(wrapper.text()).toBe('Cassel, Jory')
   })
 })
