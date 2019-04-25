d368e3979d0ff373604a8c7be1cc7faab29c3221
Tyler Clemens <Tyler.Clemens@osi.ca.gov>
2019-04-25 09:09:22 -0700

    CANS-1179 adjustments to headless selenium driver

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -56,8 +56,11 @@ end
 Capybara.register_driver :headless_selenium do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
   options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--remote-debugging-pipe')
+  options.add_argument('--incognito')
   options.add_argument('--window-size=1400,1400')
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
