66b65a2fdcdcb0d8957be5c3ecbf44729c77da0d
Amera Belay <amerabelay@cans.local>
2019-04-25 15:51:41 -0700

    font size increased for progress-value

--- app/javascript/Application/components/Assessment/DomainScore.js
+++ app/javascript/Application/components/Assessment/DomainScore.js
@@ -6,7 +6,22 @@ import './style.sass'
 const DomainScore = props => {
   return (
     <div className={'domain-score'}>
-      <Typography style={{ fontSize: '0.75rem', alignSelf: 'center', whiteSpace: 'nowrap' }}>Domain Total:</Typography>
+      <Typography
+        style={{
+          fontSize: '0.9375rem',
+          alignSelf: 'center',
+          whiteSpace: 'nowrap',
+          width: '5.5rem',
+          height: '1.125rem',
+          color: '#6F6F6F',
+          fontFamily: 'SourceSansPro - Regular',
+          fontWeight: 'normal',
+          lineHeight: '1.125rem',
+          marginRight: '0.2rem',
+        }}
+      >
+        Domain Total:
+      </Typography>
       <Badge color={'info'} className={'domain-score-badge'}>
         {props.totalScore}
       </Badge>
