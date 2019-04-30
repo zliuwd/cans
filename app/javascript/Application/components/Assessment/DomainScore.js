74ec1e9e30963a7636e5b8967985aa85214edcfc
Amera Belay <amerabelay@cans.local>
2019-04-30 08:25:37 -0700

    refactored domain score typography styles moved to sass

--- app/javascript/Application/components/Assessment/DomainScore.js
+++ app/javascript/Application/components/Assessment/DomainScore.js
@@ -6,22 +6,7 @@ import './style.sass'
 const DomainScore = props => {
   return (
     <div className={'domain-score'}>
-      <Typography
-        style={{
-          fontSize: '0.9375rem',
-          alignSelf: 'center',
-          whiteSpace: 'nowrap',
-          width: '5.5rem',
-          height: '1.125rem',
-          color: '#6F6F6F',
-          fontFamily: 'SourceSansPro - Regular',
-          fontWeight: 'normal',
-          lineHeight: '1.125rem',
-          marginRight: '0.2rem',
-        }}
-      >
-        Domain Total:
-      </Typography>
+      <Typography className={'domain-score-typography'}>Domain Total:</Typography>
       <Badge color={'info'} className={'domain-score-badge'}>
         {props.totalScore}
       </Badge>
